4b76cb52d596ae93cd6194c56261c46cfbc46a41
Ольга <141999010+olga1234lanina@users.noreply.github.com>
2023-09-08 21:01:02 +0500

    Update Program.cs
    задача решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// задача 25. написать цикл, который принимает на вход два числа (А и В) и возводит  число А в натульную степень В
+// 3, 5 -> 243 (3⁵) 
+// 2, 4 -> 16 
+
+Console.Clear();
+Console.Write("Введите назначаемое число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{A}, {B} -> {GetStepen(A,B)}");
+
+double GetStepen(int A, int B)
+{
+    double result = Math.Pow(A,B);
+    return result;
+}
